652e80a844f8860b27211298383836850916964b
Antonio <antoniobocanegragalindez@gmail.com>
2018-01-14 21:54:27 -0500

    Object.create

--- puntosMetodo.js
+++ puntosMetodo.js
@@ -1,4 +1,4 @@
-function Punto(x, y) {
+/*function Punto(x, y) {
     this.x = x
     this.y = y
 }
@@ -16,11 +16,34 @@ Punto.prototype.distancia = function distancia(p) {
     const y = this.y - p.y
 
     return Math.sqrt(x * x + y * y)
+}*/
+
+const Punto = {
+    init: function init(x, y) {
+        this.x = x
+        this.y = y
+    },
+    moverEnX: function moverEnX(x) {
+        this.x += x
+    },
+    moverEnY: function moverEnY(y) {
+        this.y += y
+    },
+    distancia: function distancia(p) {
+        const x = this.x - p.x
+        const y = this.y - p.y
+
+        return Math.sqrt(x * x  + y * y)
+    }
 }
 
-const p1 = new Punto(0, 4)
 
-const p2 = new Punto(3, 0)
+const p1 = Object.create(Punto)
+
+const p2 = Object.create(Punto)
+
+p1.init(0, 4)
+p2.init(3, 0)
 
 
 console.log(p1.distancia(p2))
