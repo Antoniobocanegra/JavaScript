dee2e73b23bc4b5e262ac7ade139841a29fd1cdc
Antonio <antoniobocanegragalindez@gmail.com>
2018-01-22 11:31:51 -0500

    Callbacks

---
+++ callbacks.js
@@ -0,0 +1,11 @@
+
+setTimeout(function callback() {
+    console.log(`Ya pasó 3 segundos`)
+}, 3000)
+
+console.log(`Hola`)
+
+setTimeout(function callback2() {
+    console.log(`ya pasó 2 segundos`)
+
+}, 2000)
